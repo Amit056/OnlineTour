6acef23be8c47e2ba2d9f2bc2902c9fe68374b98
akray <akray0413@gmail.com>
2022-12-20 10:19:11 +0530

    new Commit

---
+++ src/test/java/com/Tourism/Login/Practice.java
@@ -0,0 +1,12 @@
+package com.Tourism.Login;
+
+import org.testng.annotations.Test;
+
+public class Practice {
+	@Test
+	public void test()
+	{
+	System.out.println("practice is storednin repository");
+	}
+
+}
